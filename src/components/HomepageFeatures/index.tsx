c709762cdf635c2b84582b2b269c19ba5d5d09b6
Dominik Harz <dominik@interlay.io>
2023-03-11 16:58:14 +0900

    docs: add more details to homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -12,6 +12,8 @@ const FeatureList: FeatureItem[] = [
     title: 'About me',
     description: (
       <>
+        Hi! I'm Dominik "Dom" Harz. I am a software engineer and researcher. I am passionate about building systems that are secure, scalable, and accessible. I am currently working on Bitcoin DeFi infrastructure at <a href="https://interlay.io">Interlay</a>.
+        <br />      
         I believe that through collaboration we can build better societies. Blockchains enable collaboration on a global scale between humans and machines: obtaining an identity and the ability to interact is permissionless. The integration of on-chain identities and programmable money allows us to build entirely new systems.
         <br />
         These new systems should be transparent in that they enforce financial rules and interactions that cannot be cheated. However, they also come with great challenges as we navigate the issues of "intentions" of systems and their actual implementation.
@@ -22,7 +24,7 @@ const FeatureList: FeatureItem[] = [
     title: 'My Work',
     description: (
       <>
-        I&apos;m the co-founder and CTO of <a href="https://interlay.io">Interlay</a> where we build Bitcoin DeFi infrastructure. We build and launched the first decentralized Bitcoin bridge. The bridge is based on the <a href="https://xclaim.io">XCLAIM</a>protocol that <a href="https://www.alexeizamyatin.me/">Alexei</a> and I developed during our PhDs. We are now building out lending protocols, AMMs, and core bridging infrastructure.
+        I&apos;m the co-founder and CTO of <a href="https://interlay.io">Interlay</a> where we build Bitcoin DeFi infrastructure with an incredible team. We built and launched the first decentralized Bitcoin bridge. The bridge is based on the <a href="https://xclaim.io">XCLAIM</a>protocol that <a href="https://www.alexeizamyatin.me/">Alexei</a> and I developed during our PhDs. We are now building out lending protocols, AMMs, and core bridging infrastructure.
         <br />
         I did my PhD at the <a href="https://www.imperial.ac.uk/cryptocurrency">Imperial College London cryptocurrency lab</a> where I met some of the greatest people I had ever the chance to work with on DeFi, cross-chain, and smart contracts.
       </>
@@ -35,7 +37,7 @@ const FeatureList: FeatureItem[] = [
         When I&apos;m not in front of a computer, you will find me outdoors chasing waves, doing triathlons, hiking, or skiing.
       </>
     )
-  },
+  }
 ];
 
 function Feature({title, description}: FeatureItem) {
